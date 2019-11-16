13a7f5a741217f8219f14f1fa8d8abbf575f9ef0
dogydev <41112844+dogydev@users.noreply.github.com>
2019-11-16 20:13:04 +0000

    Commit2

--- albert/run_classifier_sp.py
+++ albert/run_classifier_sp.py
@@ -83,9 +83,9 @@ flags.DEFINE_integer(
     "Sequences longer than this will be truncated, and sequences shorter "
     "than this will be padded.")
 
-flags.DEFINE_bool("do_train", False, "Whether to run training.")
+flags.DEFINE_bool("do_train", True, "Whether to run training.")
 
-flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
+flags.DEFINE_bool("do_eval", True, "Whether to run eval on the dev set.")
 
 flags.DEFINE_bool(
     "do_predict", False,
